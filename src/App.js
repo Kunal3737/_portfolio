5a0ffde5b5c2ef18e022351160c9e559c5673888
unknown <kunalshinde373@gmail.com>
2020-07-27 01:09:12 +0530

    Save changes

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
     <>
       <CssBaseline />
       <Route exact path="/" component={Home} />
+      <Route exact path="/_portfolio" component={Home} />
       <Route path="/resume" component={Resume} />
       <Route path="/portfolio" component={Portfolio} />
       <Route path="/contacts" component={Contacts} />
