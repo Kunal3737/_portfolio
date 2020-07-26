9c3a9877e83a13513920ceb8eaecb99413001c13
unknown <kunalshinde373@gmail.com>
2020-07-27 01:12:46 +0530

    Updates

---
+++ precache-manifest.83da56d59e2528758b60cc8cf020d543.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e235a17b7e086a3be0d03b72dd7c697a",
+    "url": "/_portfolio/index.html"
+  },
+  {
+    "revision": "28ab2b7bb80cb948345d",
+    "url": "/_portfolio/static/css/main.73859b41.chunk.css"
+  },
+  {
+    "revision": "0b088310e02189405c29",
+    "url": "/_portfolio/static/js/2.0b01ca65.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/_portfolio/static/js/2.0b01ca65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28ab2b7bb80cb948345d",
+    "url": "/_portfolio/static/js/main.fb3f4741.chunk.js"
+  },
+  {
+    "revision": "1f44db8d4cd0e252a323",
+    "url": "/_portfolio/static/js/runtime-main.9b6f3dbe.js"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "/_portfolio/static/media/avatar.7c8e5f50.png"
+  },
+  {
+    "revision": "11ec2ce6adc785bf00ae2ecce3f226d0",
+    "url": "/_portfolio/static/media/banner.11ec2ce6.jpg"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/_portfolio/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "afb6bd48649330bb44533f5d871f7adf",
+    "url": "/_portfolio/static/media/financial-portfolio-tracker-img.afb6bd48.png"
+  }
+]);
